7fef4a66b9b03cc54b83477040c4273c06f2cc31
scriptmaze <hossamfelfel200@Yahoo.com>
2024-12-29 15:32:35 -0500

    added jsonresponse import, was causing 500 error in backend

--- backend/urls.py
+++ backend/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse
 
 def home(request):
     return JsonResponse({"message": "T dans le backend la route utilisé est le root"})
