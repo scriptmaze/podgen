67d3ce41142610ba392031f7ef4823345ece7220
scriptmaze <hossamfelfel200@Yahoo.com>
2024-12-29 12:48:09 -0500

    test premier deploiement

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+import React from "react";
+import { Container, AppBar, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container>
+      <AppBar
+        position="static"
+        style={{ marginBottom: "2rem", padding: "1rem" }}
+      >
+        <Typography variant="h4" align="center">
+          My Modern Website
+        </Typography>
+      </AppBar>
+      <Typography variant="body1">
+        Welcome to my modern React-Django website!
+      </Typography>
+    </Container>
+  );
+}
+
+export default App;
