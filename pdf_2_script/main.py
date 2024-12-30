e386f311e6558d5067db90ae8b79ea5d54e4744c
Etienne Goulet <etiennegoulet19@gmail.com>
2024-12-30 15:09:10 -0500

    changes

--- pdf_2_script/main.py
+++ pdf_2_script/main.py
@@ -4,19 +4,19 @@ from script_gen import create_script
 import os
 
 def main(pdf_path, output_folder, pdf_name):
-    # print("Converting PDF to PNG images...")
-    # convert_pdf_to_png(pdf_path, output_folder)
+    print("Converting PDF to PNG images...")
+    convert_pdf_to_png(pdf_path, output_folder)
 
     print("Extracting text from images...")
-    extract_text_from_images_in_folder(output_folder, 'backend/extracted_text', pdf_name)
+    extract_text_from_images_in_folder(output_folder, 'pdf_2_script/extracted_text', pdf_name)
 
-    # print("Creating script for podcast from text...")
-    # create_script(pdf_name)
+    print("Creating script for podcast from text...")
+    create_script(pdf_name)
 
 
 if __name__ == "__main__":
-    pdf_path = 'backend/pdfs/EdPsy2009.pdf'
+    pdf_path = 'pdf_2_script/pdfs/EdPsy2009.pdf'
     pdf_name = os.path.splitext(os.path.basename(pdf_path))[0]
-    output_folder = 'backend/output_images'
+    output_folder = 'pdf_2_script/output_images'
     main(pdf_path, output_folder, pdf_name)
 
