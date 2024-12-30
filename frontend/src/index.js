d7b40b621caae641499fe38a852dd63b91e5b5dd
scriptmaze <hossamfelfel200@Yahoo.com>
2024-12-30 13:43:06 -0500

    added styling folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styling/Index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
